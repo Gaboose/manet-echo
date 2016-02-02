3c1e486d3bf952ac7835100546b15b6ffab5ceb6
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2016-02-02 18:44:04 +0000

    echoing websocket

--- main.go
+++ main.go
@@ -2,21 +2,53 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	manet "github.com/Gaboose/go-multiaddr-net"
+	ma "github.com/jbenet/go-multiaddr"
+	"io"
 	"os"
-	"runtime"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
-	bind := fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
-	fmt.Printf("listening on %s...", bind)
-	err := http.ListenAndServe(bind, nil)
+	ip := os.Getenv("OPENSHIFT_GO_IP")
+	if ip == "" {
+		ip = "127.0.0.1"
+	}
+
+	port := os.Getenv("OPENSHIFT_GO_PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	m, err := ma.NewMultiaddr(fmt.Sprintf("/ip4/%s/tcp/%s/ws/echo", ip, port))
 	if err != nil {
 		panic(err)
 	}
+
+	ln, err := manet.Listen(m)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go echo(c)
+	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "greetings from %s", runtime.Version())
+func echo(rwc io.ReadWriteCloser) {
+	for {
+		buf := make([]byte, 256)
+		n, err := rwc.Read(buf)
+		if err != nil {
+			rwc.Close()
+			return
+		}
+
+		s := fmt.Sprintf("you're the \"%s\"", string(buf[:n]))
+		_, err = rwc.Write([]byte(s))
+	}
 }
